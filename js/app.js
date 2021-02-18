79a6040744c3e3fd0e9c1a63907d39bb154da511
RohanModi06 <rohan.modi9331@gmail.com>
2021-02-18 16:05:13 +0530

    added Resume

--- js/app.js
+++ js/app.js
@@ -2,17 +2,18 @@ const navbar = document.querySelector(".navbar");
 const navbar_col = document.querySelector(".navbar-collapse");
 window.onscroll = () => {
   if (window.scrollY > 50) {
-    navbar.style.backgroundColor = "rgb(77, 196, 125)";
+    // navbar.style.backgroundColor = "rgb(77, 196, 125)";
+    navbar.style.backgroundColor = "rgb(6, 139, 60)";
   } else {
     navbar.style.backgroundColor = "rgba(0, 0, 0, 0)";
   }
 };
 
-setTimeout(() => {
-  document.querySelector("#start").style.transform = "scaleY(0)";
-}, 5);
+// setTimeout(() => {
+//   document.querySelector("#start").style.transform = "scaleY(0)";
+// }, 0);
 
-// document.querySelector("#start").style.transform = "scaleY(0)";
+document.querySelector("#start").style.transform = "scaleY(0)";
 
 (function ($) {
   function initParallax() {
@@ -36,3 +37,47 @@ setTimeout(() => {
     $("body").removeClass("offcanvas-active");
   });
 })(jQuery);
+
+// $(function () {
+//   $("a[href*=#]").bind("click", function (e) {
+//     e.preventDefault(); // prevent hard jump, the default behavior
+
+//     var target = $(this).attr("href"); // Set the target as variable
+
+//     // perform animated scrolling by getting top-position of target-element and set it as scroll target
+//     $("html, body")
+//       .stop()
+//       .animate(
+//         {
+//           scrollTop: $(target).offset().top,
+//         },
+//         600,
+//         function () {
+//           location.hash = target; //attach the hash (#jumptarget) to the pageurl
+//         }
+//       );
+
+//     return false;
+//   });
+// });
+
+// $(window)
+//   .on("scroll", function () {
+//     var scrollDistance = $(window).scrollTop();
+
+//     // Show/hide menu on scroll
+//     //if (scrollDistance >= 850) {
+//     //		$('nav').fadeIn("fast");
+//     //} else {
+//     //		$('nav').fadeOut("fast");
+//     //}
+
+//     // Assign active class to nav links while scolling
+//     $(".page-section").each(function (i) {
+//       if ($(this).position().top <= scrollDistance) {
+//         $(".navigation a.active").removeClass("active");
+//         $(".navigation a").eq(i).addClass("active");
+//       }
+//     });
+//   })
+//   .on("scroll");
